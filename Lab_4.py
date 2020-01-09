026eb0c4ba98f2f3a29907a806512217b86c5ab0
kriegchris <chriskrieg95@gmail.com>
2020-01-09 09:40:59 -0500

    refactored cubed, squared and main function

--- Lab_4.py
+++ Lab_4.py
@@ -1,13 +1,35 @@
 import sys
 
 def squared(n):
-    pass
+    myList = []
+    
+    for number in range(1, n+1):
+        myList.append(number**2)
+    
+    return myList
 
 def cubed(n):
-    pass
+    myList = []
+    
+    for number in range(1, n+1):
+        myList.append(number**3)
+        
+    return myList
 
 def main(args):
-    return 0
+    print("Learn your squares and cubes!")
+    cont = "y"
+    while (cont == "y"):
+        num = input("Enter an integer: ")
+        num = int(num)
+        print('{:<10s}{:>4s}{:>12s}'.format("Number", "Squared", "Cubed"))
+        print('{:<10s}{:>4s}{:>12s}'.format("======", "=======", "====="))
+        squared_list = squared(num)
+        cubed_list = cubed(num)
+        for nums in range(0, len(squared_list)):
+            print('{:<10d}{:>4d}{:>12d}'.format(nums+1, squared_list[nums], cubed_list[nums]))
+        cont = input("Continue? (y/n): ")
+    
 
 if __name__ == '__main__':
     
